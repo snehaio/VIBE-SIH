b6140c8fc2aa67e78be70a0a2fe8655a77250bad
ParichayMago <parichaymago@gmail.com>
2024-09-08 00:01:03 +0530

    linked api

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import Navbar from './components/Navbar';
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    
     <App />
   </React.StrictMode>
 );
