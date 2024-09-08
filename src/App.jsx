367ac55475a13e7b168962324431d4f0f2cbb5c7
HarshDhiman <142768740+harshDhiman3344@users.noreply.github.com>
2024-09-08 17:06:57 +0530

    Footer at bottom

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,17 @@
-import React from 'react';
-import Navbar from './components/Navbar';
-import HeroSection from './components/HeroSection';
-import CardHolder from './components/CardHolder'; 
-import AboutUs from './components/AboutUs.jsx';
-import Testimonials from './components/Testimonials.jsx';
-import './App.css'; 
-import loco from './hooks/locoscroll.js';
-import cursorEffect from './hooks/cursorEffect.js';
-import { useEffect } from 'react';
-import Foot from './components/Footer/Foot.jsx';
+import React from "react";
+import Navbar from "./components/Navbar";
+import HeroSection from "./components/HeroSection";
+import CardHolder from "./components/CardHolder";
+import AboutUs from "./components/AboutUs.jsx";
+import Testimonials from "./components/Testimonials.jsx";
+import "./App.css";
+import loco from "./hooks/locoscroll.js";
+import cursorEffect from "./hooks/cursorEffect.js";
+import { useEffect } from "react";
+import Foot from "./components/Footer/Foot.jsx";
 
-
-export const base_url = "3.109.1.79:8080"
-export const port_number = ""
+export const base_url = "3.109.1.79:8080";
+export const port_number = "";
 
 const App = () => {
   useEffect(() => {
@@ -22,27 +21,32 @@ const App = () => {
 
   return (
     <div>
-    <div id="cursor"></div>
-    <div id="main" data-scroll-container>
-      <div id="page1" data-scroll data-scroll-speed="-5" className="bg-cover bg-center" style={{ backgroundImage: "url('https://mir-s3-cdn-cf.behance.net/project_modules/fs/0030d0169252255.64495c063cd40.png')" }}>
-        <Navbar/>
-        <HeroSection />
+      <div id="cursor"></div>
+      <div id="main" data-scroll-container>
+        <div
+          id="page1"
+          data-scroll
+          data-scroll-speed="-5"
+          className="bg-cover bg-center"
+          style={{
+            backgroundImage:
+              "url('https://mir-s3-cdn-cf.behance.net/project_modules/fs/0030d0169252255.64495c063cd40.png')",
+          }}
+        >
+          <Navbar />
+          <HeroSection />
+        </div>
+        <CardHolder />
+        <div className="App">
+          <AboutUs />
+          <Testimonials />
+        </div>
+        <div id="footSection">
+          <Foot />
+        </div>
       </div>
-      <CardHolder />
-      <div id='footSection'>
-      <Foot/>
-      </div>
-      <div className="App">
-     
-    
-        <AboutUs />
-      
-        <Testimonials /> 
-      </div>
-      </div>
-      </div>
-  
+    </div>
   );
-}
+};
 
 export default App;
