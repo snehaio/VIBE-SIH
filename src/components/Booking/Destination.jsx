b9a2108a0bde4db7c12824b634c98b4798f9e7ae
ParichayMago <parichaymago@gmail.com>
2024-09-08 18:15:08 +0530

    Destination

--- src/components/Booking/Destination.jsx
+++ src/components/Booking/Destination.jsx
@@ -85,7 +85,7 @@ const DestinationComponent = () => {
       const resp = await axios.post(`http://${base_url}/api/user/mood/destination`, userData);
       console.log("backend api resp: ", resp);
       console.log("Resp Data: ", resp.data.data);
-      setRecommendatedData(resp.data.data || []);
+      resp.data.data.length > 0 ? setRecommendatedData(resp.data.data) : setRecommendatedData(data)
     } catch (e) {
       console.log("some err", e);
     }
